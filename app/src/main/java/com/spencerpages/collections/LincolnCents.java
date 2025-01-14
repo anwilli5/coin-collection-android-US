5303cdd39aec12743db8ea5bf5e8f0aaf1415135
adesertrat <adesertrat@proton.me>
2025-01-13 21:01:01 -0700

    included 2024 update fix for lincoln cents, jefferson nickels,roosevelt dimes, kennedy halfs, and silver eagles.deleted duplicate lines from lincoln cents.renamed clad kennedy to Clad Kennedy Half Dollars and washington silver to Washington Silver Quarters as requested. fixed include strings and attr string as requested. Declined to change large cents. no images available at this time to fix 2024 quarter updates.

--- app/src/main/java/com/spencerpages/collections/LincolnCents.java
+++ app/src/main/java/com/spencerpages/collections/LincolnCents.java
@@ -1,22 +1,5 @@
 /*
- * Coin Collection, an Android app that helps users track the coins that they've collected
- * Copyright (C) 2010-2016 Andrew Williams
- *
- * This file is part of Coin Collection.
- *
- * Coin Collection is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Coin Collection is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Coin Collection.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 /*
  * Coin Collection, an Android app that helps users track the coins that they've collected
@@ -312,6 +295,10 @@ public class LincolnCents extends CollectionInfo {
         if (oldVersion <= 19) {
             // Add in new 2023 coins if applicable
             total += DatabaseHelper.addFromYear(db, collectionListInfo, 2023);
+        }
+        if (oldVersion <= 20) {
+            // Add in new 2024 coins if applicable
+            total += DatabaseHelper.addFromYear(db, collectionListInfo, 2024);
         }
 
         return total;
